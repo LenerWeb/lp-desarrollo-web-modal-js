969a622eeaa3847146ddd2801d3a9fc0008700e6
ElliotXLeo <elliotgaramendi@gmail.com>
2022-03-14 11:20:44 -0500

    Update JS

--- js/index.js
+++ js/index.js
@@ -1,59 +1,55 @@
 'use strict';
 const documentReady = () => {
-  // Start header
-  const headerScroll = () => {
-    const header = document.querySelector('.header');
-    const headerNav = document.querySelector('.header-nav');
-    const headerNavMenuIcon = document.querySelector('.header-nav__menu-icon');
-    const headerNavLinks = [...document.querySelectorAll('.header-nav__link')];
 
+  // Start header
+  const header = document.querySelector('.header');
+  const headerNav = document.querySelector('.header-nav');
+  const headerNavMenuIconContainer = document.getElementById('headerNavMenuIconContainer');
+  const headerNavMenuIcon = document.querySelector('.header-nav__menu-icon');
+  const headerNavCloseIconContainer = document.getElementById('headerNavCloseIconContainer');
+  const headerNavLinkList = document.querySelector('.header-nav__link-list');
+  const headerNavLinks = [...document.querySelectorAll('.header-nav__link')];
+
+  const documentScroll = () => {
     header.classList.toggle('header--scroll', window.scrollY > 0);
     headerNav.classList.toggle('header-nav--scroll', window.scrollY > 0);
     headerNavMenuIcon.classList.toggle('header-nav__menu-icon--scroll', window.scrollY > 0);
-    headerNavLinks.map((element) => {
+    headerNavLinks.forEach((element) => {
       element.classList.toggle('header-nav__link--scroll', window.scrollY > 0);
     });
   };
 
   const openMenu = () => {
-    const navLinkList = document.querySelector('.header-nav__link-list');
-    navLinkList.classList.add('header-nav__link-list--open');
+    headerNavLinkList.classList.add('header-nav__link-list--open');
   };
 
   const closeMenu = () => {
-    const navLinkList = document.querySelector('.header-nav__link-list');
-    navLinkList.classList.remove('header-nav__link-list--open');
+    headerNavLinkList.classList.remove('header-nav__link-list--open');
   };
 
-  const navMenuIcon = document.querySelector('.header-nav__menu-icon');
-  const navCloseIcon = document.querySelector('.header-nav__close-icon');
-  const headerNavLinkItems = [...document.querySelectorAll('.header-nav__link-item')];
-
-  window.addEventListener('scroll', headerScroll);
-  navMenuIcon.addEventListener('click', openMenu);
-  navCloseIcon.addEventListener('click', closeMenu);
-  headerNavLinkItems.map((element) => {
+  document.addEventListener('scroll', documentScroll);
+  headerNavMenuIconContainer.addEventListener('click', openMenu);
+  headerNavCloseIconContainer.addEventListener('click', closeMenu);
+  headerNavLinks.forEach((element) => {
     element.addEventListener('click', closeMenu);
   });
   // End header
 
   // Start modal
-  const coverButton = document.querySelector('.cover__button');
-  const modalButton = document.querySelector('.modal__button');
-  const modal = document.querySelector('.modal');
+  const heroMessageButton = document.getElementById('heroMessageButton');
+  const modalButton = document.getElementById('modalButton');
+  const modal = document.getElementById('modal');
 
   const openModal = () => {
     modal.classList.add('modal--show');
   };
 
-  const closeModal = () => {
+  const claseModal = () => {
     modal.classList.remove('modal--show');
   };
 
-  coverButton.addEventListener('click', openModal);
-
-  modalButton.addEventListener('click', closeModal);
+  heroMessageButton.addEventListener('click', openModal);
+  modalButton.addEventListener('click', claseModal);
   // End modal
-};
-
+}
 document.addEventListener('DOMContentLoaded', documentReady);
